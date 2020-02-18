0437c4ffbbe7df9c8e933d1c601a554549be87de
Roman Yakovlev <tensegrity666@eicloud.com>
2020-02-18 18:34:16 +0300

    feat: implement carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,16 @@
-const MODERN_ACTIVITY= 15; 
+const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
+module.exports = function dateSample(sample) {
+  if (typeof sample !== 'string' || sample === undefined || sample <= 0 || sample > HALF_LIFE_PERIOD || Math.floor(sample) === MODERN_ACTIVITY) {
+    return false;
+  }
+
+  sample = parseFloat(sample);
+  const K = 0.693 / HALF_LIFE_PERIOD; //!tests failed when uses Math.LN2
+  const T = Math.ceil((Math.log(MODERN_ACTIVITY / sample)) / K);
+
+  return isNaN(T) ? false : T;
+
   throw 'Not implemented';
-  // remove line with error and write your code here
 };
