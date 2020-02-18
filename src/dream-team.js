4dc75be8638d123494c40db49db03bc0aa098b23
Roman Yakovlev <tensegrity666@eicloud.com>
2020-02-18 22:56:29 +0300

    feat: complete dreamteam

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,14 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (members === undefined || !Array.isArray(members) || members.length === 0) {
+    return false;
+  }
+  let teamName = [];
+  members.forEach(name => {
+    if (typeof name === 'string') {
+      return teamName.push(name.trim()[0]);
+    } else {
+      return false;
+    }
+  });
+  return teamName = teamName.toString().toUpperCase().split(',').sort().join('').toString();
 };
