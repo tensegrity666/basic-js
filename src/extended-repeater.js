57de3a374c7da7fc995a2ffeaed166f882881f3b
Roman Yakovlev <tensegrity666@eicloud.com>
2020-02-19 13:35:18 +0300

    feat: basic implementation

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,8 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    const { repeatTimes, addition, additionRepeatTimes, separator } = options;
+    let additionRepeated = addition.repeat(additionRepeatTimes) + separator;
+    console.log(additionRepeated);
+    let stringRepeated = str + additionRepeated;
+
+    return stringRepeated = stringRepeated.repeat(repeatTimes);
 };
-  
