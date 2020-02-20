94a9058ca09dfa447c6ce28b888d81629f804fe9
Roman Yakovlev <tensegrity666@eicloud.com>
2020-02-20 20:44:43 +0300

    fix: implement, but very tricky moment

--- src/what-season.js
+++ src/what-season.js
@@ -2,20 +2,16 @@ module.exports = function getSeason(date) {
   if (!date) {
     return 'Unable to determine the time of year!';
   };
-  if (typeof date !== 'Object') {
-    throw new Error();
-  }
 
-  if (date.getMonth() === (11 || 0 || 1)) {
-    return 'winter';
+  const seasons = {
+    winter: [11, 0, 1],
+    spring: [2, 3, 4],
+    summer: [5, 6, 7],
+    autumn: [8, 9, 10]
   };
-  if (date.getMonth() === (2 || 3 || 4)) {
-    return 'spring';
-  }
-  if (date.getMonth() === (5 || 6 || 7)) {
-    return 'summer';
-  }
-  if (date.getMonth() === (8 || 9 || 10)) {
-    return 'autumn';
-  }
+
+  for (let season in seasons) {
+    if (seasons[season].includes(date.getMonth()))
+      return season;
+  };
 };
