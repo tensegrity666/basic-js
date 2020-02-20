7291fa8f45d06aeab914a2bdbcae399623ad8d99
Roman Yakovlev <tensegrity666@eicloud.com>
2020-02-20 20:23:29 +0300

    feat: start task

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,21 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (!date) {
+    return 'Unable to determine the time of year!';
+  };
+  if (typeof date !== 'Object') {
+    throw new Error();
+  }
+
+  if (date.getMonth() === (11 || 0 || 1)) {
+    return 'winter';
+  };
+  if (date.getMonth() === (2 || 3 || 4)) {
+    return 'spring';
+  }
+  if (date.getMonth() === (5 || 6 || 7)) {
+    return 'summer';
+  }
+  if (date.getMonth() === (8 || 9 || 10)) {
+    return 'autumn';
+  }
 };
