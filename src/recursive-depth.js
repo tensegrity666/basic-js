2fe75df92ea0c560d33b8391d4faf2d84489ee41
Roman Yakovlev <tensegrity666@eicloud.com>
2020-02-23 19:45:36 +0300

    feat: implement with toFlat

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,4 +1,15 @@
 module.exports = class DepthCalculator {
     calculateDepth(arr) {
+        let depth = 0;
+
+        arr.forEach(element => {
+            if (!Array.isArray(element)) {
+                return depth = 1;
+            } else {
+                depth++;
+                return arr = arr.flat();
+            }
+        });
+        return depth;
     }
 };
