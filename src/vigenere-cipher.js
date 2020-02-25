284b4811daeac2c70f3a60028425501e101575fa
Roman Yakovlev <tensegrity666@eicloud.com>
2020-02-25 13:54:24 +0300

    refactor: refactoring encrypt method

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -5,18 +5,30 @@ class VigenereCipheringMachine {
             throw Error('Error!');
         }
 
-        const encripted = [];
+        let encriptedMessage = [];
+        let cryptoKey = [];
+        const BASE = 26;
+
+        while (message.length > key.length) {
+            key = key.concat(key);
+        };
+
         for (let i = 0; i < message.length; i++) {
-            encripted.push(message.charCodeAt(i));
-        }
-
-        const cryptoKey = [];
-        for (let i = 0; i < key.length; i++) {
             cryptoKey.push(key.charCodeAt(i));
+            encriptedMessage.push(message.charCodeAt(i));
+            encriptedMessage[i] += cryptoKey[i] % BASE;
         }
 
-        console.log(encripted.join(','));
-        console.log(cryptoKey.join(','));
+        encriptedMessage = encriptedMessage.join(',');
+
+        console.log(String.fromCharCode(112));
+        console.log(key.charCodeAt(1));
+        console.log(message.charCodeAt(0));
+        console.log(encriptedMessage);
+        console.log(cryptoKey);
+        console.log(cryptoKey.length);
+        return encriptedMessage;
+
     };
 
     decrypt(encryptedMessage, key) {
