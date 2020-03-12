854842473148e15c27d33da15c40e6da2f847058
Roman Yakovlev <tensegrity666@eicloud.com>
2020-03-13 01:54:13 +0300

    fix: fix errors in repeat

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,8 +1,37 @@
 module.exports = function repeater(str, options) {
-    const { repeatTimes, addition, additionRepeatTimes, separator } = options;
-    let additionRepeated = addition.repeat(additionRepeatTimes) + separator;
-    console.log(additionRepeated);
-    let stringRepeated = str + additionRepeated;
+    if (typeof str !== 'string') {
+        str = str.toString();
+    }
+    if (str === '') {
+        return false;
+    }
 
-    return stringRepeated = stringRepeated.repeat(repeatTimes);
+    if (options.separator === undefined) {
+        options.separator = '+';
+    }
+    if (options.additionSeparator === undefined) {
+        options.additionSeparator = '|';
+    }
+    if (options.repeatTimes === undefined) {
+        options.repeatTimes = 1;
+    }
+    if (options.additionRepeatTimes === undefined) {
+        options.additionRepeatTimes = 1;
+    }
+    if (options.addition === undefined) {
+        options.addition = '';
+    }
+
+    if (typeof options.addition !== 'string') {
+        options.addition.toString();
+    };
+
+
+    const addSeparate = options.addition + options.additionSeparator;
+    const addRepeat = addSeparate.repeat(options.additionRepeatTimes);
+    const lastAddSeparator = addRepeat.lastIndexOf(options.additionSeparator);
+    const repeatWithSeparator = addRepeat.slice(0, lastAddSeparator) + options.separator;
+    const stringToRepeat = str + repeatWithSeparator;
+
+    return stringToRepeat.repeat(options.repeatTimes);
 };
